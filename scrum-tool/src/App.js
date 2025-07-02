8d2ffc842124ef2e8a17318ba1c77a0c1671a938
Rafael Santana Rodrigues <138138154+RafaelSR44@users.noreply.github.com>
2025-07-02 15:57:37 -0300

    x

---
+++ scrum-tool/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ScrumTool from './components/ScrumTool';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ScrumTool />
+    </div>
+  );
+}
+
+export default App;
